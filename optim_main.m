d0817c48f0e4417ea5c5a2c60491168207f69609
Abinay-Brown <abinay.joel@gmail.com>
2023-05-13 00:39:28 -0400

    Minor Change

--- optim_main.m
+++ optim_main.m
@@ -13,7 +13,7 @@ lb = [100; -pi*ones(NUM,1); -(pi/2)*ones(NUM,1); 0*ones(NUM,1); 100*ones(NUM,1);
 ub = [2000; pi*ones(NUM,1); (pi/2)*ones(NUM,1); 15e4*ones(NUM,1); 7000*ones(NUM,1); (pi/2)*ones(NUM,1); (pi/2)*ones(NUM,1); (pi/2)*ones(NUM,1)];
 load IC.mat;
 % Options for fmincon
-options = optimoptions(@fmincon, 'TolFun', 0.001, 'MaxIter', 10000, 'MaxFunEvals', 400000, 'Display', 'iter', 'DiffMinChange', 0.001, 'Algorithm', 'sqp');
+options = optimoptions(@fmincon, 'TolFun', 0.001, 'MaxIter', 10000, 'MaxFunEvals', 400000, 'Display', 'iter', 'DiffMinChange', 0.000001,'Algorithm', 'sqp');
 
 % Solve for the best simulation time + control input
 optimal = fmincon(@objective_func, x0, A, b, Aeq, Beq, lb, ub, @constraint_func, options);
